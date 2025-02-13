ef7a66108e2acbdd2f9ce683b2a998768e2e4cb0
AkaramVamshi <akaramvamshi@gmail.com>
2025-02-14 00:50:26 +0530

    Update data.js

--- user-data/data.js
+++ user-data/data.js
@@ -1,6 +1,5 @@
 export const bio = [
-  "Hello, I'm Vinay Somawat!",
-  "<strong>Frontend Software Engineer</strong> with 4+ years of experience building scalable, user-focused web applications.",
+  "Hello, I'm Akaram Vamshi!",
   "Skilled in Angular, TypeScript, and performance optimization, I aim to leverage my expertise in delivering innovative solutions that drive measurable business outcomes.",
   "Passionate about mentoring, teamwork, and continuous improvement in engineering processes.",
 ];
